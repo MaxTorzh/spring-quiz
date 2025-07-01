a90176c31579021d9f3fd79aad38b6565c3a2a54
Nia <eugeniamashina@gmail.com>
2025-07-01 18:48:26 +0300

    feat: add yaml

--- src/main/java/ru/yandex/practicum/quiz/service/ReportGenerator.java
+++ src/main/java/ru/yandex/practicum/quiz/service/ReportGenerator.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.quiz.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.quiz.model.QuizLog;
 
@@ -8,6 +9,11 @@ import java.util.List;
 
 @Component
 public class ReportGenerator {
+    private final String quizTitle;
+
+    public ReportGenerator(@Value("${spring-quiz.title}") String title) {
+        this.quizTitle = title;
+    }
     public void generate(QuizLog quizLog) {
         // Создаём объект PrintWriter, выводящий отчет в консоль
         try (PrintWriter writer = new PrintWriter(System.out)) {
@@ -19,7 +25,7 @@ public class ReportGenerator {
     }
 
     private void write(QuizLog quizLog, PrintWriter writer) {
-        writer.println("Отчет о прохождении теста \"Тест по Spring Framework\".\n");
+        writer.println("Отчет о прохождении теста " + quizTitle + "\n");
         for (QuizLog.Entry entry : quizLog) {
             // Записываем номер вопроса и текст вопроса
             writer.println("Вопрос " + entry.getNumber() + ": " + entry.getQuestion().getText());
